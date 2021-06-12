3ac7088245dee6c64128c1c9df8f289f1a939ca2
(kondo tatsuya) <(tacchann@i.softbank.jp)>
2021-06-12 12:05:44 +0900

    頭文字と末尾の削除の追加

--- main.js
+++ main.js
@@ -7,18 +7,37 @@
 const sampleForm = document.getElementById("sampleForm");
 const textCounter = document.getElementById("textCounter");
 const resetBtn = document.getElementById("resetBtn");
+const iniDel = document.getElementById("iniDel");
+const endDel = document.getElementById("endDel");
 const goalNum = 400;
+
 
 textCounter.textContent = `あと${goalNum}`;
 
 sampleForm.addEventListener("keyup", () => {
-    textLength = sampleForm.value.length;
+    textarea = sampleForm.value;
+    textLength = textarea.length;
     textCounter.textContent = `あと${goalNum - textLength}`;
+
 });
 
 resetBtn.addEventListener("click", () => {
     textCounter.textContent = `あと${goalNum}`;
+    textarea = '';
 });
+
+iniDel.addEventListener("click", () => {
+    textarea = textarea.slice(1);
+    sampleForm.value = textarea;
+    textCounter.textContent = `あと${goalNum - sampleForm.value.length}`;
+});
+
+endDel.addEventListener("click", () => {
+    textarea = textarea.slice(0, -1);
+    sampleForm.value = textarea;
+    textCounter.textContent = `あと${goalNum - sampleForm.value.length}`;
+});
+
 
 
 
